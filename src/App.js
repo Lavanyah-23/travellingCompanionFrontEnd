1bacf3b767fc39d31c020ca42ef95d6565bcf828
lesmils <miltontsie@hotmail.com>
2022-03-21 13:07:44 +0100

    commit to pull

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
-
+import io from "socket.io-client";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Loading from "./components/Loading";
@@ -11,11 +11,12 @@ import TripList from "./pages/TripList";
 import TripDetails from "./pages/TripDetails";
 import UserPage from "./pages/UserPage";
 import AddTrip from "./pages/AddTrip";
-
 import { useDispatch, useSelector } from "react-redux";
 import { selectAppLoading } from "./store/appState/selectors";
 import { getUserWithStoredToken } from "./store/user/actions";
 import { Jumbotron } from "react-bootstrap";
+
+const socket = io.connect("http://localhost:4001");
 
 const Home = () => (
   <Jumbotron>
--- src/pages/TripList/index.js
+++ src/pages/TripList/index.js
@@ -6,9 +6,11 @@ import { selectTrips } from "../../store/trips/selectors";
 import TripComponent from "../../components/TripComponent/TripComponent";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { selectToken } from "../../store/user/selectors";
 
 export default function TripList() {
   const dispatch = useDispatch();
+  const token = useSelector(selectToken);
 
   useEffect(() => {
     dispatch(fetchTrips());
@@ -21,9 +23,13 @@ export default function TripList() {
   return (
     <div>
       <h2>All Trips</h2>
-      <Link to="/addtrip">
-        <Button>Add Trip</Button>
-      </Link>
+      {!token ? (
+        "Sign in to post a trip"
+      ) : (
+        <Link to="/addtrip">
+          <Button>Add Trip</Button>
+        </Link>
+      )}
       {allTrips.map((trip, index) => {
         return <TripComponent key={index} trip={trip} />;
       })}
